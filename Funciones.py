8633255cad85ba364fdc325dce20342121381247
19760968-cmd <19760968@clases.edu.sv>
2026-07-01 11:47:46 -0600

    Add greeting and summation function in Funciones.pyMi archivo

---
+++ Funciones.py
@@ -0,0 +1,7 @@
+ef saludar_y_sumar(nombre, num1, num2=0):
+    print("saludos", nombre)
+    print("resultado de la suma", num1+num2)
+
+saludar_y_sumar("jose", 10, 5)
+saludar_y_sumar("emer", 2)
+saludar_y_sumar("jere", 800, 5000)
