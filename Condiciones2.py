62431b231cc75ea9f5491565d3807c22e57a9805
19760968-cmd <19760968@clases.edu.sv>
2026-07-01 11:44:42 -0600

    Mi archivo de condicionales 2

---
+++ Condiciones2.py
@@ -0,0 +1,16 @@
+print("Escribe t nombre:")
+nombre = input()
+#print("Escribe tu edad")
+#edad = int(input())
+
+#elif
+#Operadores logicos
+#and (y) / or (o)
+#and: Todas las expreciones sean true
+#or: Con que una de las expreciones sea true
+#> <    >=  <=
+
+if nombre == "emerson" or nombre == "onexis":
+    print("me gusta tu nombre")
+else:
+    print("Que nombre tan extraño")
