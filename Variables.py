33c2eefa19c0a8b01b0c7bd672f307385b2c019b
19760968-cmd <19760968@clases.edu.sv>
2026-07-01 11:52:25 -0600

    Mi archico

---
+++ Variables.py
@@ -0,0 +1,13 @@
+#Soy un comentario
+#strings, cadenas, textos
+#numericos
+nombre = "emerson"
+apellido = "reyes"
+edad = 16
+print(nombre, apellido, edad)
+
+#edad = edad /3
+
+print(nombre, apellido, edad)
+
+print(type(edad))
