020b65c1dc8b213e1d2ce205cfdc9d490bc4e4e9
19760968-cmd <19760968@clases.edu.sv>
2026-07-01 11:45:37 -0600

    Mi archivo condicionales

---
+++ Condicionales.py
@@ -0,0 +1,12 @@
+#Logico Boolean Boleano
+#verdadero / Falso: True / False
+#dato = 5 == 5
+#> < ==
+
+print("Ingresa tu nombre")
+nombre = input()
+
+if nombre == "emerson":
+    print("Bienvenido emerson")
+else:
+    print("Que nombre tan extraño")
